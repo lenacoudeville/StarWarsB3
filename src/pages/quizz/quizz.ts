a05361b1cefea1aab1ea0e6938502e763bc46a56
Unknown <teofilo.jeandot@gmail.com>
2018-01-09 17:01:26 +0100

    Fichiers Initiaux avec structure

---
+++ src/pages/quizz/quizz.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-quizz',
+  templateUrl: 'quizz.html'
+})
+export class QuizzPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { QuizzPage } from '../quizz/quizz';
+import { WikiPage } from '../wiki/wiki';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = QuizzPage;
+  tab3Root = WikiPage;
+  tab4Root = AboutPage;
+  tab5Root = ContactPage;
+
+  constructor() {
+
+  }
+}
